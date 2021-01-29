b93543865eb217827d9449f1e6dda8b09ffa5787
Fabio Calcagno <fcalcagno@ciandt.com>
2021-01-29 16:30:02 -0300

    try 555

--- src/main/java/com/fabio/projetodofabio/controllers/LancamentoController.java
+++ src/main/java/com/fabio/projetodofabio/controllers/LancamentoController.java
@@ -31,7 +31,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/lancamentos")
 @NoArgsConstructor
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin(origins = "*")
 public class LancamentoController {
 
     private static final Logger log = LoggerFactory.getLogger(LancamentoController.class);
--- src/main/java/com/fabio/projetodofabio/security/config/WebSecurityConfig.java
+++ src/main/java/com/fabio/projetodofabio/security/config/WebSecurityConfig.java
@@ -9,16 +9,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsUtils;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
@@ -52,10 +55,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                .anyRequest().authenticated()
-                .and().httpBasic();
+        httpSecurity.csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+                .antMatchers("/auth/**", "/api/cadastrar-pj", "/api/cadastrar-pf", "/v2/api-docs",
+                        "/swagger-resources/**", "/api/lancamentos/**", "/configuration/security", "/swagger-ui.html", "/webjars/**")
+                .permitAll().anyRequest().authenticated();
+        httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.headers().cacheControl();
     }
 
 }
